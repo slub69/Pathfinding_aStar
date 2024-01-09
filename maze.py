015c86d6be4c327903e2cbca425e1ba620abab47
slub69 <slublins2@gmail.com>
2024-01-09 10:04:42 -0500

    Revert "1/9/24"
    This reverts commit 180235b47a5cbde93249813ff96be1dfd37bb736.

--- maze.py
+++ maze.py
@@ -240,7 +240,7 @@ class WilsonMazeGenerator:
 
 ################
                 
-gen = WilsonMazeGenerator(32,16)
+gen = WilsonMazeGenerator(25,25)
 gen.generate_maze()
 gen.solve_maze()
 with open('transfer.txt', 'w') as file:
